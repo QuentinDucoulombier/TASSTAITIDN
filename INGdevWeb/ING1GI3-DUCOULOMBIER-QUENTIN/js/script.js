f6fc6579b4bcda0aad899a8a09b4ba2063d72226
ducoulombi <ducoulombi@cy-tech.fr>
2023-03-03 13:05:21 +0100

    Ca avance

---
+++ INGdevWeb/ING1GI3-DUCOULOMBIER-QUENTIN/js/script.js
@@ -0,0 +1,19 @@
+
+function agrandir(img)
+{
+    img.style.width = "10em";
+}
+
+function reduire(img)
+{
+    img.style.width = "8em";
+}
+
+function hideStock()
+{
+    let stock = document.getElementsByClassName("stock");
+    for (let i = 0; i < stock.length; i++) {
+        stock[i].style.display = "none";
+    }   
+}
+
